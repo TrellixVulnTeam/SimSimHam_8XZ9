1655865c286c2b3b75f531a51c27cf62c6ff3712
Jhun24 <jjanghuni102@naver.com>
2018-01-20 19:03:45 +0900

    upload finish

--- app.js
+++ app.js
@@ -5,14 +5,36 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+var multer = require('multer');
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'uploads/')
+    },
+    filename: function (req, file, cb) {
+        var date = new Date();
+        var month = date.getMonth();
+        var dt = date.getDate();
+        var min = date.getMinutes();
+        var year = date.getFullYear();
+        var hour = date.getHours();
+        var sec = date.getSeconds();
+
+        console.log(" Year : "+year);
+        console.log(" Month : "+sec)
+        cb(null,year+"."+month+"."+dt+"."+hour+"."+min+"."+sec+"."+file.originalname)
+    }
+})
+
+var upload = multer({ 'storage': storage})
 
 var app = express();
 
 // view engine setup
+
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'html')
+app.set('views', 'views')
+app.engine('html', require('ejs').renderFile);
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -22,8 +44,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
+require('./routes/upload')(app , upload);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -40,7 +61,6 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
 });
 
 module.exports = app;
---
+++ routes/upload.js
@@ -0,0 +1,16 @@
+/**
+ * Created by janghunlee on 2018. 1. 20..
+ */
+
+module.exports = upload;
+
+function upload(app , upload) {
+    app.get('/upload',function (req,res) {
+        res.render('yeah.html');
+    });
+
+    app.post('/upload/file', upload.single('file'), function(req, res){
+        console.log(req.file);
+        res.send('Uploaded! : '+req.file);
+    });
+}
